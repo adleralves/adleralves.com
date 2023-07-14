823cdd97fbb839ac0d5ac5bc416987da0be993c8
Adler <adler.alves@hotmail.com>
2023-07-13 23:31:53 -0300

    removendo componente header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
-import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { ProjectListComponent } from './projects/project-list/project-list.component';
 import { ProjectsComponent } from './projects/projects.component';
@@ -12,7 +11,6 @@ import { ProjectsComponent } from './projects/projects.component';
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
     ProjectsComponent,
     HomeComponent,
     FooterComponent,
